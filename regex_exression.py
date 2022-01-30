e4e430e23a1c23b041c9ff005a2417398cc8fc2b
NATHAN SHEPARD <nathan@NATHANs-MacBook-Air.local>
2022-01-30 15:25:38 -0500

    this is deleted for testing

--- regex_exression.py
+++
@@ -1,9 +0,0 @@
-# not rocket science I just want to lay this out
-# I'd like to move the file into my github and then
-# take it with my airbook and change it and then
-# move it to the next one.
-
-print("\n>>>>>>>>>>>.this is the regex_espretions file in my \n ")
-
-
-print('adding item to this right now Sunday 1:13')
