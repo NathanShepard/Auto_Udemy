9581269e9db45895683a4aac9d1eb576344e5a3e
NathanShepard <45462632+NathanShepard@users.noreply.github.com>
2022-01-30 13:01:30 -0500

    this is test 1:01

--- regex_exression.py
+++ regex_exression.py
@@ -1,1 +1,6 @@
-# to do
+# not rocket science I just want to lay this out
+# I'd like to move the file into my github and then
+# take it with my airbook and change it and then
+# move it to the next one.
+
+print("\n>>>>>>>>>>>.this is the regex_espretions file in my \n ")
