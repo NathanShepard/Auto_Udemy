4e39da27760190ca38b37de92981700f1781bf3a
NathanShepard <45462632+NathanShepard@users.noreply.github.com>
2022-01-30 15:54:03 -0500

    Update regex_exression.py
    Notes for what needs to happen now.

--- regex_exression.py
+++ regex_exression.py
@@ -1,5 +1,8 @@
-# ok lets remaber how to spell items that we need? I think
-def sub(x,y):
-    return x - y
+# this is a Python practice file for AutoMate the easystuff with python.. 
+# I can write the items I think are important in this file / #
+# try the items out in the file.. and then rework them later? in the office machine? 
+# I gues this is how this works? You work on the projects on yout local machine and then push the file up to the remote repository. and then pull it back down 
+# when you are ready to work on an other machine.. 
+# this means that with little of no trouble you can work from most anywhere? 
+# thats truly very cool..!!!
 
-print(sub(5, 2))
