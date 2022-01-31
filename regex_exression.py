9b0405da908b98bbb0d11f0e68c3eeb8fb77bddb
NATHAN SHEPARD <nathan@NATHANs-MacBook-Air.local>
2022-01-30 20:34:34 -0500

    this is the addition of auto-easy-starting

--- regex_exression.py
+++ regex_exression.py
@@ -1,8 +1,23 @@
-# this is a Python practice file for AutoMate the easystuff with python.. 
-# I can write the items I think are important in this file / #
-# try the items out in the file.. and then rework them later? in the office machine? 
-# I gues this is how this works? You work on the projects on yout local machine and then push the file up to the remote repository. and then pull it back down 
-# when you are ready to work on an other machine.. 
-# this means that with little of no trouble you can work from most anywhere? 
-# thats truly very cool..!!!
+import re, pyperclip
 
+
+#the regular espresstion  for the phone numbers
+
+phoneRegex = re.compile(r'''
+# 415-555-0000, 555-0000, (415) 555-0000 ext 12345, ext. 12345, x12345
+(
+((\d\d\d)|(\(\d\d\d)))?         #area code(oprional)
+(\s|-)                          #first separator
+\d\d\d                          #first 3 digits
+-                               #separator
+\d\d\d\d                       #last for numbers
+(((ext(\.?\s)|x)               #extention number
+(\d(2,5)))?                    #extension numbers
+)
+''', re.VERBOSE)
+
+# how do I run this on a text file?
+
+phoneRegex.find('filename?')
+
+#the email regular espresstion..
